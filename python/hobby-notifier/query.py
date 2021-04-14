71e03b2cbbb658b7f6229167bf85bf34fef099cf
Jackson Klopp <jklopp@paytrace.com>
2021-04-13 20:41:09 -0700

    somewhat finalized config.py

--- python/hobby-notifier/query.py
+++ python/hobby-notifier/query.py
@@ -1,5 +1,5 @@
 from reddit_oath import *
-import re
+import config
 
 #set keywords
 keywords = ["russian"]
@@ -11,16 +11,12 @@ headers = authenticate()
 
 requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
 
-res = requests.get("https://oauth.reddit.com/r/ComblocMarket/new",
+res = requests.get(config.hobby["subreddit"],
                 headers=headers)
 for post in res.json()['data']['children']:
         queryList.append((post['data']['title']))
 
 print(queryList)
 
-#results = [x for x in queryList if all(re.search("\\b{}\\b".format(w), x) for w in keywords)]
-
-#print(results)
-
-if '[WTB] Romy Palmswell HG’s - $40 +/- [VA]' in queryList:
-        print("test")
+#if '[WTB] Romy Palmswell HG’s - $40 +/- [VA]' in queryList:
+        #print("test")
