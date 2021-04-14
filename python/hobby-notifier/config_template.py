2365bebceb0d869c948f32caa2083f78e80ed645
Jackson Klopp <jklopp@paytrace.com>
2021-04-13 20:41:59 -0700

    somewhat finalized config.py

--- python/hobby-notifier/config_template.py
+++ python/hobby-notifier/config_template.py
@@ -4,3 +4,8 @@ auth = {
     "access_key": "your app script access key",
     "secret_key": "your app script secret key"
 }
+
+#input the subreddit and keyword you want to search for
+hobby = {
+    "subreddit": "the subreddit you want to monitor"
+}
